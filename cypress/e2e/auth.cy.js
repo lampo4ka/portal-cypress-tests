4d8757bba85907f28f63741cc16eb337005e4056
Sugonyak Kristina <sugonyak@tutu.tech>
2025-01-15 21:42:34 +0100

    added valid/invalid user tests

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,5 +1,38 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('User login and logout', () => {
+  beforeEach(() => {
+    cy.fixture('login-data.json').as('loginData')
   })
+  it("should redirect to the home page after login", () => {
+   
+      cy.get('@loginData').then((data) => {
+          const { username, password } = data.validUser
+          // cy.session([username, password], () => {
+            cy.login({username, password});
+            // cy.contains('Acknowledged').click()
+            // cy.url().should('contain', '/account/dashboard')
+      // })
+    })
+  });
+
+  it("should redirect to the login page after logout", () => {
+    cy.get('@loginData').then((data) => {
+      const { username, password, name } = data.validUser
+      cy.login({username, password});
+      cy.contains(name).click();
+      cy.contains("Logout").click()
+      cy.url().should("contain", "/login");
+    })
+  });
+
+  it("should display capcha", () => {
+      cy.get('@loginData').then((data) => {
+        const { username, password } = data.invalidUser
+        cy.login({username, password, isValidUser: false});
+        cy.contains("Security Check").should('be.visible')
+        cy.get('[placeholder="Enter above word(s)"]')
+          .type("username")
+        cy.contains('Login').click()
+    })
+
+  });
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,14 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', ({username, password, isValidUser = true}) => {
+      cy.visit('https://wave-trial.getbynder.com/login/')
+      cy.contains('Close this dialog')
+        .should('be.visible')
+        .wait(300)
+        .click()
+      cy.get('[placeholder="Email/Username"]').type(username)
+      cy.get('[placeholder="Password"]').type(password)
+      cy.contains('Login').click()
+      if(isValidUser) {
+        cy.url().should('contain', '/account/dashboard')
+      }
+      
+  })
