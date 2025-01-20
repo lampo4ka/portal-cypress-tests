c5decf1e8b596375d1817ffecf81dd6829f0d328
Sugonyak Kristina <sugonyak@tutu.tech>
2025-01-20 23:27:42 +0100

    fixed reports

--- portal-ui-tests/cypress.config.js
+++ portal-ui-tests/cypress.config.js
@@ -5,21 +5,10 @@ module.exports = defineConfig({
         baseUrl: 'https://wave-trial.getbynder.com/',
         specPattern: [
             'cypress/e2e/*.cy.js',
-            'cypress/api/top-rated-movies.cy.js',
-            'cypress/api/save-movie-rate.cy.js',
         ],
-        reporter: 'junit',
+        reporter: 'cypress-multi-reporters',
         reporterOptions: {
-            mochaFile: 'results/test-results-[hash].xml',
-            outputs: true,
-            testCaseSwitchClassnameAndName: true,
+            configFile: 'reporter-config.json',
         },
-        setupNodeEvents(on, config) {
-            // implement node event listeners here
-        },
-    },
-
-    env: {
-        apiUrl: 'https://api.themoviedb.org/3/movie',
     },
 });
